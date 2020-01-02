b4d62162bf649a283e08f7e07e5b2429f3326ea8
Minjae Shin <minjaes91@gmail.com>
2020-01-02 16:02:13 -0500

    update timePicker for android 10

--- src/TimePicker.js
+++ src/TimePicker.js
@@ -76,7 +76,6 @@ export default class TimePicker extends React.Component<Props, State> {
           data={hours}
           onItemSelected={this.onHourSelected}
           selectedItem={selectedHourIndex}
-          initPosition={selectedHourIndex}
         />
         <WheelPicker
           style={styles.wheelPicker}
@@ -85,7 +84,6 @@ export default class TimePicker extends React.Component<Props, State> {
           data={minutes}
           onItemSelected={this.onMinuteSelected}
           selectedItem={selectedMinuteIndex}
-          initPosition={selectedMinuteIndex}
         />
         {!this.props.format24 && this.renderAm()}
       </View>
